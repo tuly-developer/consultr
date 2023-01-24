4df93f441b70c898c011af1b62769226f91c5de5
tuly-developer <118399680+tuly-developer@users.noreply.github.com>
2023-01-24 10:36:33 -0300

    Initial commit

--- app/Http/Controllers/Api/SuperheroController.php
+++ app/Http/Controllers/Api/SuperheroController.php
@@ -10,9 +10,13 @@ use App\Http\Requests\SuperheroRequest;
 class SuperheroController extends Controller
 {
 
-    public function list($id = null)
+    public function list(Request $request, $id = null)
     {
+        if($request->header('api-superheroes-key') == env('API_SUPERHEROES_KEY')){
             return $id ? Superhero::with('race', 'eyeColor', 'hairColor', 'publisher')->findOrFail($id) : Superhero::all();
+        }else {
+            return 403;
+        }
     }
 
     public function getSuperheroesFilter(SuperheroRequest $request)
